6ea5e813b38f05f96dd4ebf64bbf6b7cf054487d
Abigiya Tadesse <Abigiyatadesse23@gmail.com>
2022-11-25 13:27:37 +0300

    Test failes b/c raise for ArgumentError is not set

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -5,5 +5,8 @@ describe  Solver  do
         it "should return 1 when given 0" do
             expect(Solver.factorial(0)).to eq(1)
         end
+        it "should only accept zero or positive integers" do
+            expect{Solver.factorial(-1)}.to raise_error(ArgumentError)
+        end
     end
 end
