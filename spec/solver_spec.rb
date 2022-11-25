d017af4a20c5c8320267eefb9113f010ad68a507
Abigiya Tadesse <Abigiyatadesse23@gmail.com>
2022-11-25 13:34:00 +0300

    Create more test for factorial

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -8,5 +8,8 @@ describe  Solver  do
         it "should only accept zero or positive integers" do
             expect{Solver.factorial(-1)}.to raise_error(ArgumentError)
         end
+        it "should return 100 when given 5" do
+            expect(Solver.factorial(5)).to eq(120)
+        end
     end
 end
