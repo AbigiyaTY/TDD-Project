8ba806f763c88ae8df39eade003a053b6758c0cf
Abigiya Tadesse <Abigiyatadesse23@gmail.com>
2022-11-25 13:29:54 +0300

    Test passes b/c raise for ArgumentError is set

--- solver.rb
+++ solver.rb
@@ -1,6 +1,7 @@
 class Solver
     def self.factorial(n)
         return 1 if n == 0
+    raise ArgumentError, 'This method only accepts 0 and positive integers' if n < 0
         n * factorial(n-1)
     end
 end
