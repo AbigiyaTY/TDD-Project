c89e0dd56b32e697cfde0cb109030ce1a359b306
Bantealem <bantem2021@gmail.com>
2022-11-25 15:36:43 +0300

    make fizzbuzz method

--- solver.rb
+++ solver.rb
@@ -1,10 +1,20 @@
 class Solver
-    def self.factorial(n)
-        return 1 if n == 0
-    raise ArgumentError, 'This method only accepts 0 and positive integers' if n < 0
-        n * factorial(n-1)
-    end
-    def self.reverse(str)
-        str.reverse
-    end
+  def self.factorial(number)
+    return 1 if number.zero?
+    raise ArgumentError, 'This method only accepts 0 and positive integers' if number.negative?
+
+    number * factorial(number - 1)
+  end
+
+  def self.reverse(str)
+    str.reverse
+  end
+
+  def self.fizzbuzz(num)
+    return 'fizzbuzz' if (num % 3).zero? && (num % 5).zero?
+    return 'fizz' if (num % 3).zero?
+    return 'buzz' if (num % 5).zero?
+
+    num.to_s
+  end
 end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,45 +1,45 @@
 require './solver'
 
-describe  Solver  do
-    describe '.factorial' do
-        it "should return 1 when given 0" do
-            expect(Solver.factorial(0)).to eq(1)
-        end
-        it "should only accept zero or positive integers" do
-            expect{Solver.factorial(-1)}.to raise_error(ArgumentError)
-        end
-        it "should return 100 when given 5" do
-            expect(Solver.factorial(5)).to eq(120)
-        end
+describe Solver do
+  describe '.factorial' do
+    it 'should return 1 when given 0' do
+      expect(Solver.factorial(0)).to eq(1)
     end
-    describe '.reverse' do
-        it "should return 'olleh' when given 'hello'" do
-            expect(Solver.reverse('hello')).to eq('olleh')
-        end
+    it 'should only accept zero or positive integers' do
+      expect { Solver.factorial(-1) }.to raise_error(ArgumentError)
     end
-    describe '.fizzbuzz' do
-        context 'When N is divisible by 3' do
-            it 'should return "fizz"' do 
-              expect(Solver.fizzbuzz(3)).to eq('fizz')
-            end
-        end
-        
-        context 'When N is divisible by 5' do
-            it 'should return "buzz"' do
-                expect(Solver.fizzbuzz(5)).to eq('buzz')
-            end
-        end
+    it 'should return 100 when given 5' do
+      expect(Solver.factorial(5)).to eq(120)
+    end
+  end
+  describe '.reverse' do
+    it "should return 'olleh' when given 'hello'" do
+      expect(Solver.reverse('hello')).to eq('olleh')
+    end
+  end
+  describe '.fizzbuzz' do
+    context 'When n is divisible by 3' do
+      it 'should return "fizz"' do
+        expect(Solver.fizzbuzz(3)).to eq('fizz')
+      end
+    end
 
-        context 'When N is divisible by 3 and 5' do
-            it 'should return "fizzbuzz"' do
-                expect(Solver.fizzbuzz(15)).to eq('fizzbuzz')
-            end
-        end
-
-        context 'Any other case' do
-            it 'should return N as a string' do
-                expect(Solver.fizzbuzz(7)).to eq('7')
-            end
-        end
+    context 'When n is divisible by 5' do
+      it 'should return "buzz"' do
+        expect(Solver.fizzbuzz(5)).to eq('buzz')
+      end
     end
+
+    context 'When n is divisible by 3 and 5' do
+      it 'should return "fizzbuzz"' do
+        expect(Solver.fizzbuzz(15)).to eq('fizzbuzz')
+      end
+    end
+
+    context 'Any other case' do
+      it 'should return n as a string' do
+        expect(Solver.fizzbuzz(7)).to eq('7')
+      end
+    end
+  end
 end
